3367cc81a4cf97240af7b2cd2b2af38e974d0af4
Janejira P <janejira1989@gmail.com>
2026-02-28 05:13:40 +0000

    Update new function

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 ## this is first script
 print("Hello world!")
 print("I am using github with posit.cloud")
+
+def hello():
+  name = input("What's your name: ")
+  print(f"Hello {name}!")
+  
+## test function
+hello()
